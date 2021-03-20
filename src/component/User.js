6eb2d5b2481a5db2fd68c3155768e909cfc67311
DeWill404 <mr.irshad.siddique@gmail.com>
2021-03-20 22:00:34 +0530

    iterate to all branch in firbase

--- src/component/User.js
+++ src/component/User.js
@@ -5,6 +5,37 @@ import { useEffect, useState } from "react";
 // Get database
 const db = firebase.database();
 
+
+function iteraDate(id, path, key, obj) {
+	// Iterate throught map insde the mat to lowest point
+	if (typeof(obj) == 'object') {
+		return (
+			Object.keys(obj).map(function(keyName, keyIndex) {
+				return (
+					<div key={keyName}>{ 
+						iteraDate(id, (key ? path?`${path}/${key}`:key : ''), keyName, obj[keyName]) 
+					}</div>
+				);
+			}) 
+		);
+	}
+
+	// Print out the input tag
+	return (
+		<div key={key}>
+			<label>{path ? `${path}/${key}` : key}</label>
+			<br />
+			<input value={obj}
+				onChange={e =>
+					// Update DB
+					db.ref(`data/${id}/${path}`).update({[key] : e.target.value})
+				}
+			/>
+		</div>
+	);
+}
+
+
 // Page on User side for verify details
 function User({id}) {
 	// Create set listener of data
@@ -32,31 +63,26 @@ function User({id}) {
 	else if (!data)
 		return <NotFound />;
 
-
 	// render list of data from firebase
 	return (
 		<div> {
-			Object.keys(data).map(function(keyName, keyIndex) {
-    		return (
-					<div>
-						<label>{keyName}</label>
-						<br />
-						<input value={data[keyName]}
-							onChange={e => {
-								// Make copy of date, so that it wont break sync loop
-								const copy = { ...data };
-								console.log(copy);
-								// Update data
-								copy[keyName] = e.target.value;
-								// Update DB
-								db.ref(`data/${id}`).set(copy);
-							}}
-							/>
-					</div>
-    		)
-		}) }
-		</div>
-	)
+			iteraDate(id, '', '', data)
+			// Object.keys(data).map(function(keyName, keyIndex) {
+			// 	return (
+			// 		<div key={keyName}>
+			// 			<label>{keyName}</label>
+			// 			<br />
+			// 			<input value={data[keyName]}
+			// 				onChange={e =>
+			// 					// Update DB
+			// 					db.ref(`data/${id}`).update({[keyName] : e.target.value})
+			// 				}
+			// 			/>
+			// 		</div>
+			// 	);
+			// }) 
+		} </div>
+	);
 
 }
 
