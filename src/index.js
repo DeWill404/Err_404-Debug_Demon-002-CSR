71788a71f50dd333f97e31e46935f00645085fdd
DeWill404 <mr.irshad.siddique@gmail.com>
2021-03-20 08:35:08 +0530

    firbase added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase/app'
+
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyA07lvgSJ4hs_6JbBtfRcc4IRwp8RcGbc8",
+    authDomain: "csr-app-30c29.firebaseapp.com",
+    projectId: "csr-app-30c29",
+    storageBucket: "csr-app-30c29.appspot.com",
+    messagingSenderId: "140613045209",
+    appId: "1:140613045209:web:83e3099420df6c7fbf67c8"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
